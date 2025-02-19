bd3de09a453966dde2f0d367973dc888fd1354c2
Sarang2022344 <sarangkhode456@gmail.com>
2025-02-19 12:38:58 +0530

    Sixth commit

--- src/main/java/com/asset/management/model/Company.java
+++ src/main/java/com/asset/management/model/Company.java
@@ -1,9 +1,15 @@
 package com.asset.management.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "company")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Company {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
