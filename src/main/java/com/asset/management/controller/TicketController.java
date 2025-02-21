dc79e09c9327fb63130d561f40ad87653069f8ca
Sharvari Basarkar <basarkarsharvari219@gmail.com>
2025-02-21 12:31:06 +0530

    changed controller

--- src/main/java/com/asset/management/controller/TicketController.java
+++ src/main/java/com/asset/management/controller/TicketController.java
@@ -3,8 +3,7 @@ package com.asset.management.controller;
 
 import com.asset.management.dto.TicketDTO;
 import com.asset.management.service.TicketService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("tickets")
-@Tag(name = "Ticket Management", description = "APIs for managing employee tickets")
+
 
 public class TicketController {
     private final TicketService ticketService;
@@ -24,7 +23,7 @@ public class TicketController {
     }
 
     //Raise a new maintenance ticket
-    @Operation(summary = "Create a new ticket", description = "Allows an employee to raise a new ticket")
+
     @PostMapping(value ="/employee/{employeeId}",consumes = "multipart/form-data" )
     public ResponseEntity<TicketDTO> raiseTicket(
             @PathVariable Long employeeId,
@@ -33,17 +32,15 @@ public class TicketController {
         return ResponseEntity.ok(ticketService.raiseTicket(employeeId,issueDescription,issueImageUrl));
     }
 
-
-
-
     //Get all tickets raised by an employee
-    @Operation(summary = "Get all tickets", description = "Retrieve a list of all raised tickets")
+
     @GetMapping("/employee/{employeeId}")
     public ResponseEntity<List<TicketDTO>> getTickets(@PathVariable Long employeeId) {
         return ResponseEntity.ok(ticketService.getTicketsByEmployee(employeeId));
     }
+
     //Get details of a specific ticket
-    @Operation(summary = "Get ticket by ticket id", description = "Retrieve ticket of all raised tickets")
+
     @GetMapping("/{ticketId}")
     public ResponseEntity<TicketDTO> getTicket(@PathVariable Long ticketId) {
         return ResponseEntity.ok(ticketService.getTicketById(ticketId));
