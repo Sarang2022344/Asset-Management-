29c7fd8b15d2527caec681a048f4c6aa19853e1a
Sarang2022344 <sarangkhode456@gmail.com>
2025-02-22 08:39:11 +0530

    12th commit

--- src/main/java/com/asset/management/service/TicketService.java
+++ src/main/java/com/asset/management/service/TicketService.java
@@ -6,7 +6,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 public interface TicketService {
-    TicketDTO raiseTicket(Long employeeId, String issueDescription, MultipartFile issueImageUrl);
+    TicketDTO raiseTicket(Long employeeId, String assetName, String issueDescription, MultipartFile issueImageUrl);
     List<TicketDTO> getTicketsByEmployee(Long employeeId);
     TicketDTO getTicketById(Long ticketId);
     List<TicketDTO> getAllTickets();
