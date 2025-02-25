4d8d6902b7bc8de4c88c33b9ebb12e11cfffad59
Pratz-20 <2020bec114@sggs.ac.in>
2025-02-25 15:32:21 +0530

    Resolved Conflicts

--- backend/src/main/java/com/asset/management/controller/AssetDisposalController.java
+++ backend/src/main/java/com/asset/management/controller/AssetDisposalController.java
@@ -8,6 +8,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/disposal")
 public class AssetDisposalController {
     private final AssetDisposalService disposalService;
